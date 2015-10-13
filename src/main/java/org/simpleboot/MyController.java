2dbf2734e83c0348a251b68812b4b7ef7a939cf0
Jeff Holmes <jeffaholmes@gmail.com>
2015-10-13 12:00:22 -0500

    Misc commits

--- src/main/java/org/simpleboot/MyController.java
+++ src/main/java/org/simpleboot/MyController.java
@@ -1,6 +1,6 @@
 package org.simpleboot;
 
-import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/org/simpleboot/SimpleBootApplication.java
+++ src/main/java/org/simpleboot/SimpleBootApplication.java
@@ -2,10 +2,10 @@ package org.simpleboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+//@EnableConfigurationProperties
 public class SimpleBootApplication {
 
     public static void main(String[] args) {
